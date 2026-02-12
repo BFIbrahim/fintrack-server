b51a148df412890df7726eebb7f1f6b97535c94c
Md Ibrahim <mdibrahim36194@gmail.com>
2026-02-12 11:31:57 +0600

    basic server setup completed

---
+++ index.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const dotenv = require('dotenv');
+const cors = require('cors');
+
+dotenv.config();
+
+const PORT = process.env.PORT || 5000
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+    res.send("TasteTrail Server is Running");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
