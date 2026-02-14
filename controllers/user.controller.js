cc2b9f44ac0db9f6f583222edeb1521cb4fa7e57
Md Ibrahim <mdibrahim36194@gmail.com>
2026-02-14 15:15:13 +0600

    user management api created

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,6 +1,5 @@
 const User = require("../models/User.model");
 
-// ১. কারেন্ট ইউজার গেট করা
 exports.getMe = async (req, res) => {
     try {
         const user = await User.findById(req.user.id).select("-password");
@@ -8,5 +7,31 @@ exports.getMe = async (req, res) => {
         res.status(200).json(user);
     } catch (error) {
         res.status(500).json({ message: "Server error" });
+    }
+};
+
+exports.getAllUsers = async (req, res) => {
+    try {
+        const users = await User.find({}).select("-password").sort({ createdAt: -1 });
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(500).json({ message: "Failed to load users" });
+    }
+};
+
+exports.makeAdmin = async (req, res) => {
+    try {
+        const { id } = req.params;
+        const user = await User.findByIdAndUpdate(
+            id, 
+            { role: "admin" }, 
+            { new: true }
+        );
+        
+        if (!user) return res.status(404).json({ message: "User not found" });
+        
+        res.status(200).json({ message: "Admin created", user });
+    } catch (error) {
+        res.status(500).json({ message: "Failed to make admin" });
     }
 };
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,7 +2,10 @@ const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/user.controller");
 const verifyToken = require("../middlewares/verifyToken");
-
+const {verifyAdmin} = require("../middlewares/verifyAdmin");
 router.get("/users/me", verifyToken, userController.getMe);
+
+router.get("/users", verifyToken, verifyAdmin, userController.getAllUsers);
+router.patch("/users/admin/:id", verifyToken, verifyAdmin, userController.makeAdmin);
 
 module.exports = router;
